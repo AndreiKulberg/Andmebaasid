2051847d2b8d1c5e853c8f177cc36153f438d892
AndreiKulberg <158563052+AndreiKulberg@users.noreply.github.com>
2025-05-12 11:28:59 +0300

    Update SqlTrigger.sql

--- SqlTrigger.sql
+++ SqlTrigger.sql
@@ -190,3 +190,103 @@ CREATE TABLE product_audits(
     CHECK(operation = 'INS' or operation='DEL')
 );
 
+create database kulberg
+use kulberg;
+
+CREATE TABLE firma (
+	firmaID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	firmanimi VARCHAR(20) unique,
+	aadress VARCHAR(20),
+	telefon VARCHAR(20)
+);
+
+CREATE TABLE Praktikajuhendaja (
+    praktikajuhendajaID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    eesnimi VARCHAR(50),
+    perekonnanimi VARCHAR(50),
+    synniaeg DATE,
+    telefon VARCHAR(20)
+);
+
+CREATE TABLE praktikabaas (
+		praktikabaasID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+		firmaID int,
+		praktikatingimused varchar(20),
+		arvutiprogramm varchar(20),
+		juhendajaID int,
+		FOREIGN KEY (firmaID) REFERENCES firma(firmaID),
+		FOREIGN KEY (juhendajaID) REFERENCES praktikajuhendaja(praktikajuhendajaID)
+);
+
+insert into firma (firmanimi, aadress, telefon) values ( 'gdeto', '2nd floor', '58000510');
+insert into firma (firmanimi, aadress, telefon) values ( 'kose mnt', '3th floor', '55544122');
+insert into firma (firmanimi, aadress, telefon) values ( 'anija', '4th floor', '58996767');
+insert into firma (firmanimi, aadress, telefon) values ( 'viineri', '5th floor', '58008078');
+insert into firma (firmanimi, aadress, telefon) values ( 'liivalaia', '6th floor', '56053619');
+select * from firma
+
+insert into Praktikajuhendaja(eesnimi, perekonnanimi, synniaeg, telefon) values ( 'andrei', 'kulberg', '27/02/2006', '58000510');
+insert into Praktikajuhendaja(eesnimi, perekonnanimi, synniaeg, telefon) values ( 'artur', 'kulberg', '13/04/2004', '55544122');
+insert into Praktikajuhendaja(eesnimi, perekonnanimi, synniaeg, telefon) values ( 'marek', 'kulberg', '15/10/2003', '56998078');
+
+CREATE TABLE Praktikabaas_logi (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    kasutaja VARCHAR(255) NOT NULL,
+    aeg DATETIME DEFAULT CURRENT_TIMESTAMP,
+    tegevus VARCHAR(255) NOT NULL,
+    andmed TEXT
+);
+
+select * from Praktikabaas_logi
+
+CREATE TRIGGER praktikabaas_insert
+ON praktikabaas
+FOR INSERT
+AS
+BEGIN
+    INSERT INTO Praktikabaas_logi(kasutaja, tegevus, aeg, andmed)
+    SELECT
+        SYSTEM_USER,
+        'INSERT',
+        GETDATE(),
+        CONCAT('Добавлена запись ID: ', inserted.praktikabaasID)
+    FROM inserted;
+END;
+
+CREATE TRIGGER praktikabaas_update
+ON praktikabaas
+FOR UPDATE
+AS
+BEGIN
+    INSERT INTO Praktikabaas_logi(kasutaja, tegevus, aeg, andmed)
+    SELECT
+        SYSTEM_USER,
+        'UPDATE',
+        GETDATE(),
+        CONCAT(
+            'Старая запись ID: ', deleted.praktikabaasID, 
+            ' | Новая запись ID: ', inserted.praktikabaasID
+        )
+    FROM deleted INNER JOIN inserted
+    ON deleted.praktikabaasID = inserted.praktikabaasID;
+END;
+
+CREATE TRIGGER trg_Check_Juhendaja_Synniaeg
+ON praktikajuhendaja
+INSTEAD OF INSERT
+AS
+BEGIN
+IF EXISTS (SELECT 1 FROM inserted WHERE synniaeg > GETDATE())
+BEGIN
+RAISERROR('Sünniaeg ei tohi olla tulevikus.', 16, 1);
+END
+ELSE
+BEGIN
+INSERT INTO praktikajuhendaja (eesnimi, perekonnanimi, synniaeg, telefon)
+SELECT eesnimi, perekonnanimi, synniaeg, telefon
+FROM inserted;
+END
+END;
+
+INSERT INTO praktikajuhendaja (eesnimi, perekonnanimi, synniaeg, telefon)
+VALUES ('Mari', 'Tamm', '2006-01-01', '555-1234');
